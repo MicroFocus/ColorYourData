d15e0b3cf40d61c3f2f044483a18865f13dec255
hadjiski <radi.hadzhiyski@hpe.com>
2016-06-15 13:42:38 +0000

    Add files via upload

---
+++ connectors/Twitter2BVD/Twitter2BVDConfig.py
@@ -0,0 +1,10 @@
+# change hashtags for which tweets should be sent to BVD
+hashtags = ['#hpediscover', '#hpe', '#iot', '#bigdata']
+# change how often twitter updates are pulled (in secs). Note that below 2 sec may end up in twitter refusing your requests
+sleep = 5
+# key and secret for your application-only authentication for Twitter API
+apiKey = 'foor'
+apiSecret = 'bar'
+# Url to BVD receiver and corresponding BVD api key
+bvdUrl = 'https://foo'
+bvdApiKey = 'bar'
