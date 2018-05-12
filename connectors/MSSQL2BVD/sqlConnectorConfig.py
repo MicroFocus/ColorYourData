c54f0827814ecf2d90789ec238d43c57bf58e4dd
unitedart <raoofpmajeed@gmail.com>
2018-05-12 11:21:48 +0400

    Update sqlConnectorConfig.py

--- connectors/MSSQL2BVD/sqlConnectorConfig.py
+++ connectors/MSSQL2BVD/sqlConnectorConfig.py
@@ -1,4 +1,11 @@
 
+#SQL connection parameters
+server = "10.23.67.30:1456"; # Add Server Hostname with IP to access the DB, Eg: 10.23.67.30:1456
+user = "UserName" # Add Username Here Eg: sqladmin
+password = "Password" # Password to access database
+database = "event" # database name where query need to be executed
+
+# change how often query need to be executed and send to BVD
 sleep = 5
 
 # Url to BVD receiver and corresponding BVD api key
